b30ec63988fc16f35cf7248a1cf3442fc6c701f4
Ben McCormick <mr.mccormick@Bens-MacBook-Pro.local>
2024-02-16 18:43:53 -0800

    Expand models js

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -5,6 +5,26 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "Product",
+        key: "id",
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "Tag",
+        key: "id",
+      },
+    }
   },
   {
     sequelize,
