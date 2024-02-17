f963e59b25b8896598e5a5178b7607744f39ebf7
Ben McCormick <mr.mccormick@Bens-MacBook-Pro.local>
2024-02-16 18:35:40 -0800

    Updated code spacing

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -3,10 +3,8 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
-
 Category.init(
   {
-    // define columns
   },
   {
     sequelize,
